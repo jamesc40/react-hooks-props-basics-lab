a1ba780e3c7bf24c4309ddb571dec7410b2e1bcd
James Coffman <jamesc4023@gmail.com>
2022-04-25 16:27:22 -0400

    commit tree b59cf9702789bb699d88b915e4842b7d9f2a1d33

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Links = ({github, linkedin}) => {
+    console.log(github)
+    return (
+        <>
+            <h3>Links</h3>
+            <a href={github}>{github}</a>
+            <a href={linkedin}>{linkedin}</a>
+        </>
+    )
+}
+
+export default Links;
